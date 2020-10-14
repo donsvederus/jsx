09391e2e9827c89b5d29c53b79c3402929f16dca
donsvederus <58320530+donsvederus@users.noreply.github.com>
2020-10-14 02:01:12 -0600

    0.12 Displaying Content with Functional Components
    - Displayed content on screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+////////////////////////////
 // Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+////////////////////////////
+// Create a react component 
 
-// Create a react component
+/*  Old way to do a function
 
+    const App = function() {
+    return <div>Hi there!</div>;
+    };
 
+*/
+
+// New Way, ES2015
+
+    const App = () => {
+    return <div>Hi there!</div>;
+    };
+
+//
+
+//////////////////////////////
 // Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
