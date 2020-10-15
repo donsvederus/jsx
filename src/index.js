da518e6516d10a8cddf4e921e66b101e6692cbe3
donsvederus <58320530+donsvederus@users.noreply.github.com>
2020-10-15 02:20:37 -0600

    0.17 Converting Styling to JSX Format

--- src/index.js
+++ src/index.js
@@ -17,7 +17,13 @@ import ReactDOM from 'react-dom';
 // New Way, ES2015
 
     const App = () => {
-    return <div>Hi there!</div>;
+        return (
+            <div>
+                <label class="label" for="name">Enter name: </label>
+                <input id="name" type="text" />
+                <button style={{ backgroundColor: "blue", color: "white" }}>Submit</button>
+            </div>
+        );
     };
 
 //
